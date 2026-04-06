6b40808e14f0d1cb261361d8a54f67cfa13dfaf4
wangjh <wangjianhui2003@outlook.com>
2026-04-06 22:40:07 +0800

    add: gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,5 +1,4 @@
-#include<iostream>
-#include<numeric>
+#include <bits/stdc++.h>
 using namespace std;
 
 // 辗转相除法
@@ -12,16 +11,15 @@ int gcd1(int a, int b) {
     return a;
 }
 
-int main(int argc, char const *argv[]){
-    cout<<gcd1(2117,3139)<<endl;
-}
-
-
-int gcd2(int a,int b){
-    while(b != 0){
+int gcd2(int a, int b) {
+    while (b != 0) {
         int temp = b;
         b = b % a;
         a = temp;
     }
     return a;
 }
+
+int gcd3(int a, int b) { return __gcd(a, b); }
+
+int main(int argc, char const *argv[]) { cout << gcd1(2117, 3139) << endl; }
